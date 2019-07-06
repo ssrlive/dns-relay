aa26dcb8f04852ce4c22a39616840dca715d2545
Gaojianli <823805669@qq.com>
2019-07-07 01:10:50 +0800

    remove useless connect_state

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,10 +1,8 @@
 #include "cache.h"
 cache::cache(const char* host, size_t port){
 	logger = std::unique_ptr<cpp_redis::logger>(new cpp_redis::logger);
-	client.connect(host, port, [](const std::string& host, std::size_t port, cpp_redis::client::connect_state status) {
-		if (status == cpp_redis::client::connect_state::dropped) {
-			std::cout << "client disconnected from Redis： " << host << ":" << port << std::endl;
-			exit(-1);
+	client.connect(host, port, [](const std::string& host, std::size_t port) {
+			std::cout << "Server connect to Redis： " << host << ":" << port << std::endl;
 		}
 		});
 }
